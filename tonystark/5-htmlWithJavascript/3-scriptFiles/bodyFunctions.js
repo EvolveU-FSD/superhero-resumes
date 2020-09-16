4c3ffcc0d3b474d516b3d0190fa510e6d0cc3865
Chris Desmarais <chrisdesmarais@Chriss-MacBook-Pro.local>
2020-09-16 11:32:58 -0600

    Adding more examples

---
+++ tonystark/5-htmlWithJavascript/3-scriptFiles/bodyFunctions.js
@@ -0,0 +1,14 @@
+const educationAlert = () => {
+    console.log("Are you sure you don't have more degrees, Mr. Stark?")
+}
+
+function languageCheck(language){
+    alert('Oh do you know '+language+'?' )
+}
+
+function armorCheck(){
+    const happyKnows = false
+    debugger;
+    if (!happyKnows)
+        alert("Should you really be telling everyone this Mr. Stark?")
+}
---
+++ tonystark/5-htmlWithJavascript/3-scriptFiles/headerFunctions.js
@@ -0,0 +1,9 @@
+function hello() {
+    const name = 'Tony'
+    console.log("Hello "+name)
+}
+
+function addressAlert(){ 
+    alert("Mr Stark's public address.")
+}
+
